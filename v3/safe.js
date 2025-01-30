f959c0f8a4e3a55fc6dc8a34d95fb81b137c18c0
louis <louis.chevallier@gmail.com>
2025-01-30 11:40:56 +0100

    trying-to-reproduce

--- v3/safe.js
+++ v3/safe.js
@@ -1,67 +1,108 @@
 'use strict';
 
+
+
 // eslint-disable-next-line no-unused-vars
 export class Safe {
-  #key = '';
+#key = '';
 
-  #encoder = new TextEncoder();
-  #decoder = new TextDecoder();
+#encoder = new TextEncoder();
+#decoder = new TextDecoder();
 
-  #buffer(string) {
+#buffer(string) {
     const bytes = new Uint8Array(string.length);
     [...string].forEach((c, i) => bytes[i] = c.charCodeAt(0));
     return bytes;
-  }
-
-  async open(password) {
-      this.#key = await crypto.subtle.digest({
-          name: 'SHA-256'
-      }, this.#encoder.encode(password)).then(result => crypto.subtle.importKey('raw', result, {
-          name: 'AES-CBC'
-      }, true, ['encrypt', 'decrypt']));
-      console.log(this.#key);
-      
-  }
-  export() {
-    return crypto.subtle.exportKey('raw', this.#key).then(ab => {
-      return btoa(String.fromCharCode(...new Uint8Array(ab)));
-    });
-  }
-  import(string) {/* Uint8Array */
-    const decodedKeyData = new Uint8Array(Array.from(atob(string), c => c.charCodeAt(0)));
-
-    return crypto.subtle.importKey('raw', decodedKeyData, {
-      name: 'AES-CBC'
-    }, true, ['encrypt', 'decrypt']).then(key => {
-      this.#key = key;
-    });
-  }
-  async encrypt(string) {
-    const iv = crypto.getRandomValues(new Uint8Array(16));
-
-    const result = await crypto.subtle.encrypt({
-      name: 'AES-CBC',
-      iv
-    }, this.#key, this.#encoder.encode(string));
-
-    return new Promise(resolve => {
-      const reader = new FileReader();
-      reader.onload = () => resolve(reader.result.split(',')[1]);
-      reader.readAsDataURL(new Blob([iv, result], {type: 'text/enc'}));
-    });
-  }
-  async decrypt(string) {
-    // compatibility fix
-    string = string.replace('data:application/octet-binary;base64,', '');
-
-    const iv = crypto.getRandomValues(new Uint8Array(16));
-
-    const result = await crypto.subtle.decrypt({
-      name: 'AES-CBC',
-      iv
-    }, this.#key, this.#buffer(atob(string)));
-
-    const ab = (new Uint8Array(result)).subarray(16);
-    return this.#decoder.decode(ab);
-  }
 }
+
+    async open(password) {
+        this.#key = await crypto.subtle.digest({
+            name: 'SHA-256'
+        }, this.#encoder.encode(password)).then(result => crypto.subtle.importKey('raw', result, {
+            name: 'AES-CBC'
+        }, true, ['encrypt', 'decrypt']));
+        //console.log(this.#key);
+        
+    }
+    export() {
+        return crypto.subtle.exportKey('raw', this.#key).then(ab => {
+            return btoa(String.fromCharCode(...new Uint8Array(ab)));
+        });
+    }
+    import(string) {/* Uint8Array */
+        const decodedKeyData = new Uint8Array(Array.from(atob(string), c => c.charCodeAt(0)));
+
+        return crypto.subtle.importKey('raw', decodedKeyData, {
+            name: 'AES-CBC'
+        }, true, ['encrypt', 'decrypt']).then(key => {
+            this.#key = key;
+        });
+    }
+    async encrypt(string) {
+        const iv = crypto.getRandomValues(new Uint8Array(16));
+
+        const result = await crypto.subtle.encrypt({
+            name: 'AES-CBC',
+            iv
+        }, this.#key, this.#encoder.encode(string));
+
+        return new Promise(resolve => {
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result.split(',')[1]);
+            reader.readAsDataURL(new Blob([iv, result], {type: 'text/enc'}));
+        });
+    }
+    async decrypt(string) {
+        // compatibility fix
+        string = string.replace('data:application/octet-binary;base64,', '');
+
+        const iv = crypto.getRandomValues(new Uint8Array(16));
+
+        const result = await crypto.subtle.decrypt({
+            name: 'AES-CBC',
+            iv
+        }, this.#key, this.#buffer(atob(string)));
+
+        const ab = (new Uint8Array(result)).subarray(16);
+        return this.#decoder.decode(ab);
+    }
+
+    async encrypt2(string) {
+        const iv = crypto.getRandomValues(new Uint8Array(16));
+        const result = await crypto.subtle.encrypt({
+            name: 'AES-CBC',
+            iv
+        }, this.#key, this.#encoder.encode(string));
+        console.log(result);
+        const b = new Blob([iv, result], {type: 'text/enc'});
+        console.log(b);
+
+        console.log("decoded ", this.#decoder.decode(b));
+
+        
+        const r1 = result.split(',')[1];
+        console.log(r1);
+        
+        return result;
+    }
+
+
+    
+    async test() {
+        console.log(" ----------------- testing -----------------");
+        const password = "motdepasse";
+        const text = "secret";
+        await safe.open(password);
+        console.log("aaa");
+        const encrypted = 'data:application/octet-binary;base64,' + await safe.encrypt2(text);
+
+        console.log(encrypted);
+        
+        console.log(" ----------------- end testing -----------------");        
+    }
+    
+}
+
+
+let safe = new Safe();
+safe.test();
--- v3/test.js
+++ v3/test.js
@@ -1,6 +1,6 @@
 
 
-
+import { Safe } from "./safe.js"
 
 const { subtle } = globalThis.crypto;
 
@@ -13,32 +13,30 @@ function open(password) {
     }, encoder.encode(password)).then(result => crypto.subtle.importKey('raw', result, {
         name: 'AES-CBC'
     }, true, ['encrypt', 'decrypt']));
-    console.log(key);
+    console.log('key ', key);
+
     return key;
 }
 
-
-
 (async function() {
-
-    
     const key = await subtle.generateKey({
         name: 'HMAC',
         hash: 'SHA-256',
         length: 256,
     }, true, ['sign', 'verify']);
-    
+
+    //data:application/octet-binary;base64,fWq9LDj5C8v6qkDVVYg27WF7xT35+TAujJBdgDlFo2c=
     const enc = new TextEncoder();
-    const message = enc.encode('I love cupcakes');
-    console.log(message);
+    const message = enc.encode('secret');
+    console.log('message ', message);
     const digest = await subtle.sign({
         name: 'HMAC',
     }, key, message);
-    console.log(digest);
+    console.log("digest ", digest);
 
     const key2 = await open("password");
     console.log("key2 ", key2);
-    const string = "I love cupcakes"
+    const string = "secret";
     const iv = crypto.getRandomValues(new Uint8Array(16));
 
     const result = await crypto.subtle.encrypt({
@@ -49,7 +47,35 @@ function open(password) {
     console.log('result ', result);
 
 
+    const password = "toto";
+    console.log('xxxxxxxxxxxxxxxxxxx');
+    const text = "abcdefg";
+    let safe = new Safe();
     
+    await safe.open(password);
+
+    const iv3 = safe.crypto.getRandomValues(new Uint8Array(16));    
+    const result3 = await safe.crypto.subtle.encrypt({
+        name: 'AES-CBC',
+        iv3
+    }, safe.#key, safe.#encoder.encode(string));
+    
+    console.log(result3.split(',')[1]);
+
+    //reader.readAsDataURL(new Blob([iv, result], {type: 'text/enc'}));
+    
+
+
+
+
+
+
+
+
+    
+    const encrypted = 'data:application/octet-binary;base64,' + await safe.encrypt(text);
+
+
 
     
 })(); 
